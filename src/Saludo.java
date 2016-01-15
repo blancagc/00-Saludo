acd2c7dca6c86073b2983251ce8994a9fa5092ab
blancagc <blanca.glez.c@hotmail.com>
2016-01-15 14:26:38 -0600

    00-saludo

---
+++ src/Saludo.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Blanca
+ *
+ */
+public class Saludo {
+	/**
+	 * 
+	 * @param main para mandar saludo desde ventana y consola 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//JOptionPane.showMessageDialog(null, "hola Blanca");
+		System.out.println("hola desde consola");
+
+	}
+
+}
